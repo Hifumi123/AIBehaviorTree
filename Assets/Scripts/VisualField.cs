c87a38650f03266522eaa905c0abc2da8e8c050a
Hifumi123 <you@example.com>
2021-01-09 01:53:23 +0800

    完成项目，正式提交

---
+++ Assets/Scripts/VisualField.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class VisualField : MonoBehaviour
+{
+    public Transform player;
+
+    public Transform robotModel;
+
+    private bool m_IsPlayerInRange;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - robotModel.position;
+
+            Ray ray = new Ray(robotModel.position + Vector3.up * 1.5f, direction);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.transform == player)
+                    GetComponentInParent<HoverBotAgent>().targetInVisualField = true;
+                else
+                    GetComponentInParent<HoverBotAgent>().targetInVisualField = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+            m_IsPlayerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+            m_IsPlayerInRange = false;
+    }
+}
